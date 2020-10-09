a3098c1558a709f9d38066c4e30b8b714bd7416d
TheMrL14 <lennert.vanden.herrewegen@student.ehb.be>
2020-10-09 18:09:53 +0200

    verifyingKey passed

--- src/Signature.js
+++ src/Signature.js
@@ -1,3 +1,9 @@
 module.exports = () => {
-    
+    return Object.freeze({
+        verifyingKey: () => {
+            let pk,sk
+            nacl.crypto_sign_keypair(pk,sk)
+            return pk;
+           }
+    })
 }
