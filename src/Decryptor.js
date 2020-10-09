a6826f7ef77ab8ea047ac6a633967806769e6d1d
TheMrL14 <lennert.vanden.herrewegen@student.ehb.be>
2020-10-09 16:54:16 +0200

    Decrypor test passed

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-    
+const nacl = require('libsodium-wrappers')
+
+module.exports = (key)=> {
+    if (typeof key === 'undefined') {
+        throw "no key"
+      }
+    return Object.freeze({
+        decrypt: (ciphertext, nonce) => {
+         return nacl.crypto_secretbox_open_easy(ciphertext, nonce, key);
+        }
+    })
+
 }
